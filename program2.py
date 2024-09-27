555326de798174e0324987b7e2d8a707331205e2
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 13:49:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,5 +6,22 @@ class Solution(object):
         """
         pass
 
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        result = 0
 
+        for i in range(len(s) - 1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+
+        result += roman_map[s[-1]]  # add the last numeral
+
+        return result
 
