6b3e4d8d2bb09dc361e426cbab6f4247f61ab80e
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 13:46:50 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,6 +6,23 @@ class Solution(object):
         """
         pass
 
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping.values():  # opening bracket
+                stack.append(char)
+            elif char in mapping:  # closing bracket
+                if not stack or mapping[char] != stack.pop():
+                    return False
+
+        return not stack
 
 
 
