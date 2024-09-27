b83e43d7264769f5bba3eb8e46a272a46ad20e2a
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 13:50:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,14 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         result = 0
 
